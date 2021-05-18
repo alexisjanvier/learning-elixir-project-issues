19e45ac6ef0ff8f21869c12d6f6cd9091a8aad05
Alexis Janvier <contact@alexisjanvier.net>
2021-05-18 21:24:08 +0200

    Code coverage

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       source_url: "https://github.com/alexisjanvier/learning-elixir-project-issues",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      build_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -27,7 +34,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.7"},
       {:poison, "~> 4.0"},
       {:ex_doc, "~> 0.24.2"},
-      {:earmark, "~> 1.4"}
+      {:earmark, "~> 1.4"},
+      {:excoveralls, "~> 0.14.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
